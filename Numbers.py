88ca87eac91eb696176128237fcb489aacc9c4a0
Dinu1026 <47492601+Dinu1026@users.noreply.github.com>
2020-07-10 11:43:14 +0530

    Create Numbers.py

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+ #1.) Types of Numbers in Python
+    #2.) Basic Arithmetic
+    #3.) Differences between classic division and floor division
+   # 4.) Object Assignment in Python
+   #lets start with basic aritmatic
+  
+# Addition
+2+1
+# Subtraction
+2-1
+# Multiplication
+2*2
+# Division
+3/2
+# Floor Division
+7//4
